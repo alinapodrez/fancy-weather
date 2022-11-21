a21b6d731c02523fd937bcaf1aae0948c13da4d4
alinapodrez <alinapodrez@gmail.com>
2022-11-21 19:16:44 +0300

    feat: create header

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,31 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-    'no-multiple-empty-lines': 0,
-    'react/react-in-jsx-scope': 0,
-    'template-curly-spacing': 0,
-    'indent': 2,
-    'react-hooks/exhaustive-deps': 0,
-    'linebreak-style': 0,
-  }
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    'rules': {
+        'no-multiple-empty-lines': 0,
+        'react/react-in-jsx-scope': 0,
+        // 'template-curly-spacing': 0,
+        'indent': 2,
+        'react-hooks/exhaustive-deps': 0,
+        // 'linebreak-style': 0,
+    },
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,26 @@
+import {FC} from 'react';
+import './Header.scss';
+
+
+export const Header:FC = () => {
+    return(
+        <header className="header">
+            <div className='controllers'>
+                <button className='button bg-refresh'><span></span></button>
+               
+                <select className='button language'>
+                    <option value='EN'>EN</option>
+                    <option value='RU'>RU</option>
+                </select>
+                
+                <button className='button temperature fah'>°F</button>
+                <button className='button temperature cel'>°C</button>
+            </div>
+
+            <form action="" method="get">
+                <input placeholder="Search city or ZIP" type="search"/>
+                <button className='button search' type="submit">SEARCH</button>
+            </form>
+        </header>
+    );
+}
